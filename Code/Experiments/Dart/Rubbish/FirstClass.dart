b66aefcf084aa60c58f869b97e31cfd7146aec71
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2021-12-31 11:41:13 +0100

    Dart classes

---
+++ Code/Experiments/Dart/Rubbish/FirstClass.dart
@@ -0,0 +1,9 @@
+class car {
+  //Field
+  String engine = "E1001";
+
+  //Function
+  void disp() {
+    print(engine);
+  }
+}
---
+++ Code/Experiments/Dart/Rubbish/mainFirstClass.dart
@@ -0,0 +1,11 @@
+import 'FirstClass.dart';
+
+void main() {
+  var obj = new car();
+
+  //Chiamo un attributo
+  print(obj.engine);
+
+  //Chiamo una funzione
+  obj.disp();
+}
