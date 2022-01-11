a526f1a2aafa4fc60bbdcf4086c9efaf72425492
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-01-11 22:41:26 +0100

    changed project plan

--- Code/Experiments/truccoparrucco/lib/main.dart
+++ Code/Experiments/truccoparrucco/lib/main.dart
@@ -3,14 +3,16 @@ import 'components/Login.dart';
 import 'components/Gestore.dart';
 import 'components/TravelApp.dart';
 import 'components/PrenotazioniCliente.dart';
+import 'components/Chosen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
+    home: Chosen(),
     //home: PrenotazioneCliente(),
     //home: TravelApp(),
     //home: PagGestore(),
-    home: LoginScreen(),
+    //home: LoginScreen(),
     //home: nextPage(),
     //home: Registrazione(),
   ));
