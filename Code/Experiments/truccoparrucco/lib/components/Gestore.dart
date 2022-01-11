4e48785e94727a42fb405916397fd5cd1809ccf9
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-01-11 22:54:15 +0100

    changed a widget

--- Code/Experiments/truccoparrucco/lib/components/Gestore.dart
+++ Code/Experiments/truccoparrucco/lib/components/Gestore.dart
@@ -11,7 +11,49 @@ class _PagGestoreState extends State<PagGestore> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Color(0xFFF6F7FF),
+      appBar: AppBar(
+        elevation: 4.0,
+        backgroundColor: Color(0xFFF6F7FF),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Let's start by adding the text
+            Text(
+              "Prenotazioni effettuate",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Color(0xFFB7B7B7),
+        selectedItemColor: Colors.blue,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              icon: Icon(Icons.bookmark),
+              onPressed: () {},
+            ),
+            label: "Prenotazioni",
+          ),
+        ],
+      ),
     );
   }
 }
--- Code/Experiments/truccoparrucco/lib/main.dart
+++ Code/Experiments/truccoparrucco/lib/main.dart
@@ -4,11 +4,13 @@ import 'components/Gestore.dart';
 import 'components/TravelApp.dart';
 import 'components/PrenotazioniCliente.dart';
 import 'components/Chosen.dart';
+import 'components/Gestore.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Chosen(),
+    home: PagGestore(),
+    //home: Chosen(),
     //home: PrenotazioneCliente(),
     //home: TravelApp(),
     //home: PagGestore(),
