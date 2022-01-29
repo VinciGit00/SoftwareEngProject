241af5ea1af53a2aa59df7131a57049ee524d8d2
davidguzmanp <43582853+davidguzmanp@users.noreply.github.com>
2022-01-29 19:07:47 +0100

    changes to the personal information

--- lib/Model/Entity/user.dart
+++ lib/Model/Entity/user.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
@@ -7,11 +8,27 @@ class user extends ChangeNotifier {
   late String street;
   final database = FirebaseDatabase.instance;
 
-  user._privateConstructor(); //pattern to create a singleton
-  static final user _instance = user._privateConstructor();
-  factory user() => _instance;
+  //user._privateConstructor(); //pattern to create a singleton
+  user._privateConstructor() {
+    // getUserInfo();
+  }
+  static late final user _instance = user._privateConstructor();
+  factory user() =>
+      _instance; //add thingie to get user name from server once instatiatiated
 
   Future<void> addUserInfo(String email, String nick, String street) async {
+    var directory = await database.ref('users');
+    var event = await directory.orderByChild('email').equalTo(email).once();
+
+    if (event.snapshot.value != null) {
+      Map<dynamic, dynamic> values =
+          event.snapshot.value as Map<dynamic, dynamic>;
+      values.forEach((key, values) async {
+        print(values);
+        await database.ref('users/' + values['nick']).remove();
+      });
+    }
+
     this.email = email;
     this.nick = nick;
     this.street = street;
@@ -22,6 +39,28 @@ class user extends ChangeNotifier {
       'street': street,
       'inputDate': DateTime.now().toString(),
     });
+  }
+
+  Future<void> getUserInfo() async {
+    var directory = await database.ref('users');
+    var event = await directory
+        .orderByChild('email')
+        .equalTo(FirebaseAuth.instance.currentUser!.email!)
+        .once();
+
+    if (event.snapshot.value != null) {
+      Map<dynamic, dynamic> values =
+          event.snapshot.value as Map<dynamic, dynamic>;
+      values.forEach((key, values) async {
+        print(values);
+        this.email = values['email'];
+        this.nick = values['nick'];
+        this.street = values['street'];
+      });
+    } else {
+      this.nick = 'no nick';
+      this.street = 'no street';
+    }
   }
 
   String get nickname => nick;
