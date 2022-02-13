d5ca18752842cf8eea1288e8341c1131eb93fa1f
davidguzmanp <43582853+davidguzmanp@users.noreply.github.com>
2022-02-13 16:42:18 +0100

    new diagram

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hair2/Model/Entity/clientBookings.dart';
 import 'package:hair2/authentication_service.dart';
@@ -12,9 +17,21 @@ import 'Model/Entity/stylist2.dart';
 import 'components/InterfacciaPrincipale.dart';
 import 'components/Login.dart';
 
+//Quality metrics:
+// Calcolo complessità: flutter pub run dart_code_metrics:metrics analyze lib
+// File non utilizzati: flutter pub run dart_code_metrics:metrics check-unused-files lib
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    // Replace with actual values
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDUseQaLHqDBREp79yq_JUam_toBUC0Tbc",
+      appId: "1:208777354980:web:8d869d40a6a8acebedd6f1",
+      messagingSenderId: "208777354980",
+      projectId: "hair2-9d321",
+    ),
+  );
   runApp(MyApp());
 }
 
